669431e62ae8f3821fc081a3c02c5904498eca13
svnday <40030806+svnday@users.noreply.github.com>
2022-02-04 20:47:47 -0600

    connected server to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,10 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'D3fa%um645547!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello world'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
